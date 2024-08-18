4d3e997c936b1559d93689db7796e65b2b82bea5
Yoocee Ansah <yooceeansah@gmail.com>
2024-08-18 15:24:03 +0100

    Update models.py

--- drfecommerce/drfecommerce/product/models.py
+++ drfecommerce/drfecommerce/product/models.py
@@ -32,3 +32,44 @@ class Product(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class ProductLine(models.Model):
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    sku = models.CharField(max_length=100, unique=True)
+    stock_qty = models.IntegerField(default=0)
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="product_lines"
+    )
+
+    def __str__(self):
+        return f"{self.product.name} - {self.sku} (${self.price})"
+
+
+class ProductImage(models.Model):
+    name = models.CharField(max_length=255, blank=True)
+    alternative_text = models.CharField(max_length=255, blank=True)
+    url = models.ImageField(upload_to="product_images/")
+    product_line = models.ForeignKey(
+        ProductLine, on_delete=models.CASCADE, related_name="images"
+    )
+
+    def __str__(self):
+        return self.name if self.name else f"Image for {self.product_line.sku}"
+
+
+class Attribute(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class AttributeValue(models.Model):
+    value = models.CharField(max_length=100)
+    attribute = models.ForeignKey(
+        Attribute, on_delete=models.CASCADE, related_name="values"
+    )
+
+    def __str__(self):
+        return f"{self.attribute.name}: {self.value}"
