0578162b4daf04c8820b3e4ae88e32fd1f9af8b1
Yoocee Ansah <yooceeansah@gmail.com>
2024-08-18 15:38:52 +0100

    updated views.py

--- drfecommerce/drfecommerce/product/views.py
+++ drfecommerce/drfecommerce/product/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
-from .models import Category, Brand, Product
-from .serializers import CategorySerializer, BrandSerializer, ProductSerializer
+from .models import *
+from .serializers import *
 from drf_spectacular.utils import extend_schema
 
 # Create your views here.
@@ -44,4 +44,56 @@ class ProductViewSet(viewsets.ViewSet):
     @extend_schema(responses=ProductSerializer)
     def list(self, request):
         serializer = ProductSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+
+class ProductLineViewSet(viewsets.ViewSet):
+    """
+    A simple Viewset for viewing all product lines
+    """
+
+    queryset = ProductLine.objects.all()
+
+    @extend_schema(responses=ProductLineSerializer)
+    def list(self, request):
+        serializer = ProductLineSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+
+class ProductImageViewSet(viewsets.ViewSet):
+    """
+    A simple Viewset for viewing all product images
+    """
+
+    queryset = ProductImage.objects.all()
+
+    @extend_schema(responses=ProductImageSerializer)
+    def list(self, request):
+        serializer = ProductImageSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+
+class AttributeViewSet(viewsets.ViewSet):
+    """
+    A simple Viewset for viewing all attributes
+    """
+
+    queryset = Attribute.objects.all()
+
+    @extend_schema(responses=AttributeSerializer)
+    def list(self, request):
+        serializer = AttributeSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+
+class AttributeValueViewSet(viewsets.ViewSet):
+    """
+    A simple Viewset for viewing all attributes
+    """
+
+    queryset = AttributeValue.objects.all()
+
+    @extend_schema(responses=AttributeValueSerializer)
+    def list(self, request):
+        serializer = AttributeValueSerializer(self.queryset, many=True)
         return Response(serializer.data)
