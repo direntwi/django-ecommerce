b2507e0dfe7c3074227d8fc1ceb3cf6e0b4c3989
Yoocee Ansah <yooceeansah@gmail.com>
2024-08-18 15:30:57 +0100

    Updated serializers.py

--- drfecommerce/drfecommerce/product/serializers.py
+++ drfecommerce/drfecommerce/product/serializers.py
@@ -1,5 +1,13 @@
 from rest_framework import serializers
-from .models import Brand, Category, Product
+from .models import (
+    Brand,
+    Category,
+    Product,
+    ProductImage,
+    ProductLine,
+    Attribute,
+    AttributeValue,
+)
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -22,4 +30,35 @@ class ProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Product
+        fields = "__all__"
+
+
+class ProductLineSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+
+    class Meta:
+        model = ProductLine
+        fields = "__all__"
+
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    product = ProductLineSerializer()
+
+    class Meta:
+        model = ProductImage
+        fields = "__all__"
+
+
+class AttributeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Attribute
+        fields = "__all__"
+
+
+class AttributeValueSerializer(serializers.ModelSerializer):
+    attribute = AttributeSerializer()
+
+    class Meta:
+        model = AttributeValue
         fields = "__all__"
